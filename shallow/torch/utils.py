56005a0825ab3c10bdbaa90d4bb7b6456aff9a3b
MDM988 <mmould@star.sr.bham.ac.uk>
2023-01-04 23:46:16 +0000

    training split

--- shallow/torch/utils.py
+++ shallow/torch/utils.py
@@ -1,9 +1,64 @@
+import numpy as np
 import torch
 
 
 cpu = torch.device('cpu')
 gpu = torch.device('cuda')
 device = gpu if torch.cuda.is_available() else cpu
+
+
+def training_split(n, f_train, f_valid=None, seed=None):
+    
+    assert 0 < f_train <= 1
+    n_train = int(n * f_train)
+    
+    if f_valid is not None:
+        assert 0 <= f_valid <= 1 - f_train
+    else:
+        f_valid = 1 - f_train
+    n_valid = int(n * f_valid)
+    
+    idxs = np.arange(n)
+    np.random.seed(n)
+    np.random.shuffle(idxs)
+    np.random.seed()
+    
+    train = np.sort(idxs[:n_train])
+    valid = np.sort(idxs[n_train:n_train+n_valid])
+    test = np.sort(idxs[n_train+n_valid:])
+    
+    return train, valid, test
+    
+#     assert 0 < f_train <= 1
+#     n_train = int(n * f_train)
+    
+#     n_left = n - n_train
+#     if n_left == 0:
+#         return np.arange(n), [], []
+    
+#     if f_valid is not None:
+#         assert 0 <= f_valid <= 1 - f_train
+#     else:
+#         f_valid = 1 - f_train
+#     # n_valid = int(n * f_valid)
+#     n_valid = int(n_left * f_valid / (1 - f_train))
+    
+#     idxs = np.arange(n)
+#     np.random.seed(seed)
+#     np.random.shuffle(idxs)
+#     np.random.seed()
+    
+#     train = np.sort(idxs[:n_train])
+#     valid = np.sort(idxs[n_train:n_train+n_valid])
+    
+#     n_left = n - n_train - n_valid
+#     if n_left == 0:
+#         return train, valid, []
+    
+#     n_test = n_left
+#     test = np.sort(idxs[-n_test:])
+    
+    return train, valid, test
 
 
 def get_tensor(data, dtype=torch.get_default_dtype(), device=device):
