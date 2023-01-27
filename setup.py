c803547ce25d8b4c2b0bbd3ef7494ff8fc16495c
MDM988 <mmould@star.sr.bham.ac.uk>
2023-01-27 20:26:46 +0000

    0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'shallow'
-version = '0.1.0' # current = 0.0.12
+version = '0.1.0'
 
 with open('README.md' ,'r') as f:
     long_description = f.read().strip()
