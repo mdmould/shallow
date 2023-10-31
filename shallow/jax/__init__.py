e4450adfd399f9074983d47431cd7d668d775d52
MDM988 <mattdmould@gmail.com>
2023-10-30 20:05:14 -0400

    prep flowjax interface

--- shallow/jax/__init__.py
+++
@@ -1,9 +0,0 @@
-import jax
-import jax.numpy as jnp
-import equinox
-import optax
-import jax_tqdm
-
-
-
-
---
+++ shallow/jax/flows.py
@@ -0,0 +1,15 @@
+import jax
+import jax.numpy as jnp
+import equinox
+import optax
+import jax_tqdm
+
+
+# flow wrapper
+# - bounding transform
+# - params pytree to array
+# - numerical solver for BNAF inverse
+
+# training loop
+# - scan batches and epochs
+
